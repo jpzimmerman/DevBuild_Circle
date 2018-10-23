f1d63ce35d8fa7e041d990266c6a70bf8e3b437c
Josh Zimmerman <JoshuaZimmerman@quickenloans.com>
2018-10-23 12:03:06 -0400

    This is my first commit for a basic Circle class

---
+++ DevBuild.Circle/Circle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevBuild.Circle
+{
+    class Circle
+    {
+        private double radius;
+
+        public Circle(double _circleRadius) {
+            radius = _circleRadius;
+        }
+
+        public double Radius {
+            get { return radius; }
+            set { radius = value; }
+        }
+
+
+        public double CalculateCircumference() {
+            return (2 * Math.PI * Radius);
+        }
+
+        public string CalculateFormattedCircumference() {
+            return FormatNumber(CalculateCircumference()) + " units";
+        }
+
+        public double CalculateArea() {
+            return (Math.PI * Math.Pow(Radius, 2));
+        }
+
+        public string CalculateFormattedArea() {
+            return FormatNumber(CalculateArea()) + " square units";
+        }
+
+        private string FormatNumber(double x) {
+            return Math.Round(x, 2).ToString();
+        }
+    }
+}
--- DevBuild.Circle/Program.cs
+++ DevBuild.Circle/Program.cs
@@ -3,13 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DevBuild.Utilities;
 
 namespace DevBuild.Circle
 {
-    class Program
-    {
+    class Program { 
         static void Main(string[] args)
         {
+            string userResponse; 
+            double userRadius = 0;
+            Circle userCircle;
+
+            while (true)
+            {
+                userResponse = "";
+                Console.WriteLine("Welcome to the Circle Tester  ");
+
+                while (!double.TryParse(userResponse, out userRadius) || userRadius < 0) {
+                    UserInput.PromptUntilValidEntry("Enter radius: ", out userResponse, InformationType.Numeric);
+                }
+                userCircle = new Circle(userRadius);
+                Console.OutputEncoding = Encoding.Unicode;
+                Console.WriteLine("********************************");
+                Console.WriteLine($"Circumference (2\x3C0r): {userCircle.CalculateFormattedCircumference()}");
+                Console.WriteLine($"Area (\x3C0r\x00B2): {userCircle.CalculateFormattedArea()}");
+                Console.WriteLine("********************************");
+            }
         }
     }
 }
